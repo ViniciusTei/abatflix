458e237d9da935b3b4ac6a412d5c57f056b2bdb1
Vincius <viniciustprates@gmail.com>
2021-03-28 16:25:58 -0300

    :construction: Created login and added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import Cadastrar from './components/Singup';
-
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 import { AuthProvider } from './contexts/AuthContext';
-
 import GlobalStyle from './styles/global';
+  
+import Cadastrar from './components/Singup';
+import Home from './components/Home';
+import Login from './components/Login';
 
 const App: React.FC = () => {
   
   return (
     <div>
-      <AuthProvider>
-        <Cadastrar></Cadastrar>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route path="/cadastro" exact component= {Cadastrar} />
+            <Route path="/login" exact component= {Login} />
+            <Route path="/home" exact component= {Home} />
+            <Route path="/" exact render={() => {
+              //is user authenticated?
+              let is = true
+              return is ? <Redirect to="/home"></Redirect> : <Redirect to="/login"></Redirect>
+            }} />
+          </Switch>
+        </AuthProvider>
+      
         <GlobalStyle/>
-      </AuthProvider>
-
+      </Router>
     </div>
   );
 }
